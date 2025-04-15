23844c8bf8f3dead36daafe5df3961f39a90f0df
stenioneves <stenioneves@ymail.com>
2025-04-15 16:26:26 -0300

    Criações das pastas

--- Iac.sh
+++ Iac.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 
+#############################################################################
+#    Autor: Stenio Neves                                                    #
+# Script para implatação de usuarios e grupos e pastas com seus respectivas #
+# permissões                                                                #
+#############################################################################
+
+# Criando as pastas no diretório /
+echo 'criando a pasta adm'
+mkdir /adm # Criar pasta ADM
+echo 'criando a pasta vend'
+mkdir /ven # Criar pasta VEN
+echo 'criando a pasta sec'
+mkdir /sec #Criar  pasta SEC
+echo 'criando a pasta pasta público'
+mkdir /publico # Criar pasta público
