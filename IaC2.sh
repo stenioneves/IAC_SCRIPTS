f8b8af7fc106ac9b544665341a6494b9eb8ba2f0
stenioneves <stenioneves@ymail.com>
2025-04-16 00:02:51 -0300

    Script inicial

---
+++ IaC2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Scripts para provisionar um servidor web
+
+apt update -y && apt upgrade -y
+
+apt install unzip -y
+
+apt install apache2
+
+
+#https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
